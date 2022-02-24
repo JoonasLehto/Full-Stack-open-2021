d3324a7f92186394d92662e4f7c1d119239dcb49
Joonas Lehto <joonas.eino.lehto@gmail.com>
2022-02-24 09:21:53 +0200

    Added osa4.2
    .

---
+++ osa4/bloglist/app.js
@@ -0,0 +1,24 @@
+const config = require('./utils/config')
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => console.log('connected to MongoDB'))
+  .catch((error) => console.log('error connecting to MongoDB:', error.message))
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
+})
